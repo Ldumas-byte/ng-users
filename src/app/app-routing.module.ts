b4ba96c8430b4098e2570fe8d24508078c114c95
Lamarr Dumas <dumaslamarr@gmail.com>
2020-04-09 15:30:56 -0500

    Ready for class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { UserEditComponent }   from './user-edit/user-edit.component';
+import { ArticlesComponent }   from './articles/articles.component';
+import { UserViewComponent }   from './user-view/user-view.component';
+import { UserCreateComponent }   from './user-create/user-create.component';
+import { RouterModule, Routes } from '@angular/router';
 
+// 2. Import the UserComponent
+import { UsersComponent }   from './users/users.component';
 
-const routes: Routes = [];
-
+// 3. Declare your routes
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/view/:id', component: UserViewComponent },
+  { path: 'users/create', component: UserCreateComponent },
+  { path: 'users/edit/:id', component: UserEditComponent },
+  { path: 'articles', component: ArticlesComponent }
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
